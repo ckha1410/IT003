23808e9929c9ce89c26f062c60d97df2287e55fc
ckha1410 <161996268+ckha1410@users.noreply.github.com>
2024-05-11 00:45:40 +0700

    Delete DSA-DoAnCuoiKy/IV.4.2.MakeTrieNode.cpp

--- DSA-DoAnCuoiKy/IV.4.2.MakeTrieNode.cpp
+++
@@ -1,9 +0,0 @@
-TrieNode* makeTrieNode(char data)
-{
-    TrieNode* newNode = new TrieNode();
-    for (int i = 0; i < ALPHABET_SIZE; i++)
-        newNode->children[i] = NULL;
-    newNode->is_leaf = 0;
-    newNode->data = data;
-    return newNode;
-}
