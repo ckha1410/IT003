c667a073410a63e1927fa728ce042730edaeff3b
ckha1410 <161996268+ckha1410@users.noreply.github.com>
2024-05-11 00:51:18 +0700

    Update TrieNode.cpp

--- DSA-DoAnCuoiKy/Xay_dung_cau_truc_du_lieu_Trie/TrieNode.cpp
+++ DSA-DoAnCuoiKy/Xay_dung_cau_truc_du_lieu_Trie/TrieNode.cpp
@@ -1,6 +1,6 @@
 struct TrieNode
 {
-    char data; //storing for printing purposes only
+    char data;        //storing for printing purposes only
     TrieNode* children[ALPHABET_SIZE];
-    bool is_leaf; //a flag to check if it's a leaf node
+    bool is_leaf;    //a flag to check if it's a leaf node
 };
