f9c24ce44f57c731d209e0c02ce072f5139e3e22
ckha1410 <161996268+ckha1410@users.noreply.github.com>
2024-05-11 22:56:40 +0700

    Update printTrie.cpp

--- DSA-DoAnCuoiKy/Cac_thao_tac_co_ban_tren_Trie/printTrie.cpp
+++ DSA-DoAnCuoiKy/Cac_thao_tac_co_ban_tren_Trie/printTrie.cpp
@@ -2,8 +2,11 @@ void printTrie(TrieNode* root)
 {
     //print the nodes of the trie
     if (root == NULL) return;
+    //initialize the currentNode pointer with the root node
     TrieNode* current = root;
+    //print the current character in this currentNode
     cout << "->" << current->data;
+    //iterate across the length of the children array to print the next character
     for (int i = 0; i < ALPHABET_SIZE; i++)
         printTrie(current->children[i]);
 }
