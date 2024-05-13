0d7368d6aaffa95bc2a00bd055d4e521009e1a2b
ckha1410 <161996268+ckha1410@users.noreply.github.com>
2024-05-13 23:49:23 +0700

    Update TrieNode.cpp

--- DSA-DoAnCuoiKy/Xay_dung_cau_truc_du_lieu_Trie/TrieNode.cpp
+++ DSA-DoAnCuoiKy/Xay_dung_cau_truc_du_lieu_Trie/TrieNode.cpp
@@ -1,6 +1,8 @@
+//define a trie node structure
 struct TrieNode
 {
-    char data;        //storing for printing purposes only
-    TrieNode* children[ALPHABET_SIZE];
-    bool is_leaf;    //a flag to check if it's a leaf node
+    char data; //storing for printing purposes only
+    TrieNode* children[ALPHABET_SIZE];  //pointer array for child nodes of each node
+    bool is_leaf; //a flag to check if it's a leaf node
+                  //that means it is the last character of a word in trie
 };
