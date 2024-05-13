03a77b6ada161e32f30de8c4594a8ffe76cde960
ckha1410 <161996268+ckha1410@users.noreply.github.com>
2024-05-13 23:49:48 +0700

    Update makeTrieNode.cpp

--- DSA-DoAnCuoiKy/Xay_dung_cau_truc_du_lieu_Trie/makeTrieNode.cpp
+++ DSA-DoAnCuoiKy/Xay_dung_cau_truc_du_lieu_Trie/makeTrieNode.cpp
@@ -1,9 +1,12 @@
+//function to make a new trie node
 TrieNode* makeTrieNode(char data)
 {
+    //create a trie node
     TrieNode* newNode = new TrieNode();
+    //initialize every index of childNode array with NULL
     for (int i = 0; i < ALPHABET_SIZE; i++)
         newNode->children[i] = NULL;
-    newNode->is_leaf = 0;
-    newNode->data = data;
+    newNode->is_leaf = 0;   //initialize the is_leaf with 0
+    newNode->data = data;   //initialize the data with character in node
     return newNode;
 }
