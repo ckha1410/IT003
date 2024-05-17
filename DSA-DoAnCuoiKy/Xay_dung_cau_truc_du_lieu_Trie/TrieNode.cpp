449e2ea83d74602a24f0f7ac79782ecd11a00689
ckha1410 <161996268+ckha1410@users.noreply.github.com>
2024-05-17 16:14:51 +0700

    Update TrieNode.cpp

--- DSA-DoAnCuoiKy/Xay_dung_cau_truc_du_lieu_Trie/TrieNode.cpp
+++ DSA-DoAnCuoiKy/Xay_dung_cau_truc_du_lieu_Trie/TrieNode.cpp
@@ -1,6 +1,12 @@
-//define a trie node structure
+/// xác định cấu trúc của một nút
 struct TrieNode
 {
+    /**
+     * một biến data dùng cho mục đích in cây trie
+     * mảng con trỏ cho các nút con của mỗi nút
+     * một cờ để đánh dấu là nút chứa ký tự kết thúc của từ
+    **/
+
     char data; //storing for printing purposes only
     TrieNode* children[ALPHABET_SIZE];  //pointer array for child nodes of each node
     bool is_leaf; //a flag to check if it's a leaf node
