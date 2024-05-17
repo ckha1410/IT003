7cd0f210bdf7653289f1e6fde8d3d9457f07c987
ckha1410 <161996268+ckha1410@users.noreply.github.com>
2024-05-17 16:15:52 +0700

    Update freeTrieNode.cpp

--- DSA-DoAnCuoiKy/Xay_dung_cau_truc_du_lieu_Trie/freeTrieNode.cpp
+++ DSA-DoAnCuoiKy/Xay_dung_cau_truc_du_lieu_Trie/freeTrieNode.cpp
@@ -1,6 +1,11 @@
-//function to free trie node
+/// hàm giải phóng con trỏ
 void freeTrieNode(TrieNode* root)
 {
+    /**
+     * gọi đệ quy tìm đến nút chứa ký tự cuối cùng của từ
+     * thực hiện giải phóng con trỏ
+    **/
+
     //free the trie node sequence
     for (int i = 0; i < ALPHABET_SIZE; i++)
     {
