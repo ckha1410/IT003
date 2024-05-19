a15accf06cff1798a43f7c3b0741fefc54d511e7
ckha1410 <161996268+ckha1410@users.noreply.github.com>
2024-05-19 22:07:01 +0700

    Update TrieNode.cpp

--- DSA-DoAnCuoiKy/Xay_dung_cau_truc_du_lieu_Trie/TrieNode.cpp
+++ DSA-DoAnCuoiKy/Xay_dung_cau_truc_du_lieu_Trie/TrieNode.cpp
@@ -4,7 +4,7 @@ struct TrieNode
        XÁC ĐỊNH CẤU TRÚC CỦA MỘT NÚT:
 
      * một biến data dùng cho mục đích in cây trie
-     * mảng con trỏ cho các nút con của mỗi nút
+     * mảng con trỏ cho các nút con của mỗi nút, có kích thướng là độ dài bảng chữ cái ALPHABET_SIZE = 26
      * một cờ để đánh dấu là nút chứa ký tự kết thúc của từ
 
     **/
